3d47b34372b971e8daef3b0995792ebeff707928
Aaron Stannard <aaron@petabridge.com>
2021-04-23 17:10:18 -0500

    work in progress

--- src/ClusterPingPong/src/Akka.ClusterPingPong/Actors/BenchmarkCoordinator.cs
+++ src/ClusterPingPong/src/Akka.ClusterPingPong/Actors/BenchmarkCoordinator.cs
@@ -9,5 +9,29 @@ namespace Akka.ClusterPingPong.Actors
 {
     public class BenchmarkCoordinator : UntypedActor
     {
+        protected override void OnReceive(object message){
+
+        }
+    }
+
+    // One of these runs on every node, to deploy the relevant actors
+    public sealed class BenchmarkRunner : UntypedActor{
+        private HashSet<IActorRef> _benchmarkActors = new HashSet<IActorRef>();
+        private HashSet<IActorRef> _echoActors = new HashSet<IActorRef>();
+        private readonly ILoggingAdapter _log = Context.GetLogger();
+
+        public Akka.Cluster.Cluster Cluster { get; } = Akka.Cluster.Cluster.Get(Context.System);
+
+        protected override void OnReceive(object message){
+
+        }
+
+        protected void BenchmarkRunning(object message){
+
+        }
+
+        protected override void PreStart(){
+            Cluster.Subscribe(Context.Self, );
+        }
     }
 }
--- src/ClusterPingPong/src/Akka.ClusterPingPong/Messages/BenchmarkProtocol.cs
+++ src/ClusterPingPong/src/Akka.ClusterPingPong/Messages/BenchmarkProtocol.cs
@@ -11,11 +11,45 @@ namespace Akka.ClusterPingPong.Messages
     {
         public interface IBenchmarkMsg{}
 
+        // Used to tell two nodes the establish a ping / pong pair
         public record BenchmarkToNode : IBenchmarkMsg
         {
             public Address Pinger { get; set; }
 
             public Address Pingee { get; set; }
         }
+
+        // EchoActor is ready
+        public record PingeeAck : IBenchmarkMsg{
+            public IActorRef EchoActor { get; set;}
+        }
+
+        // BenchmarkActor is ready and has received reference to EchoActor
+        public record PingerAck : IBenchmarkMsg{
+            public IActorRef EchoActor { get; set;}
+
+            public IActorRef BenchmarkActor {get;set;}
+        }
+
+        // Signal to BenchmarkActor to begin
+        public record Begin : IBenchmarkMsg{
+            public long ExpectedMessages { get; set;}
+        }
+
+        // Signal to BenchmarkCoordinator that one ping-pong leg has completed
+        public record RoundStats : IBenchmarkMsg{
+            public long ReceivedMessges { get; set;}
+
+            public TimeSpan Elapsed { get; set;}
+
+            public Address Pinger { get; set; }
+
+            public Address Pingee { get; set; }
+
+            public override string ToString() => $"BenchmarkRun(From={Pinger},To={Pingee})[MessagesRecv:{ReceivedMessages}][Elapsed:{Elapsed}]";
+        }
+
+        // Signals the end of the round
+        public sealed class RoundComplete : IBenchmarkMsg{ }
     }
 }
