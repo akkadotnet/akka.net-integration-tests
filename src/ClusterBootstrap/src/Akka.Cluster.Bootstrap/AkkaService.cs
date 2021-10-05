02f5180814622bdb418e1ed456ea2180689552d0
Aaron Stannard <aaron@petabridge.com>
2021-10-05 14:29:39 -0500

    added more stress testing to K8s discovery (#38)

--- src/ClusterBootstrap/src/Akka.Cluster.Bootstrap/AkkaService.cs
+++ src/ClusterBootstrap/src/Akka.Cluster.Bootstrap/AkkaService.cs
@@ -4,10 +4,14 @@ using System.Threading;
 using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Bootstrap.Docker;
+using Akka.Cluster.Tools.PublishSubscribe;
 using Akka.Configuration;
 using Akka.DependencyInjection;
+using Akka.Event;
 using Akka.Management;
 using Akka.Management.Cluster.Bootstrap;
+using Akka.Util;
+using Akka.Util.Internal;
 using Microsoft.Extensions.Hosting;
 using Petabridge.Cmd.Cluster;
 using Petabridge.Cmd.Host;
@@ -15,6 +19,62 @@ using Petabridge.Cmd.Remote;
 
 namespace Akka.Cluster.Bootstrap
 {
+    public sealed class ChaosActor : ReceiveActor
+    {
+        public ChaosActor()
+        {
+            Receive<int>(i =>
+            {
+                switch (i)
+                {
+                    case 1: // graceful shutdown
+                        Context.System.Terminate();
+                        return;
+                    case 2: // crash
+                        Context.System.AsInstanceOf<ExtendedActorSystem>().Abort();
+                        return;
+                    default:
+                    {
+                        // do nothing
+                        break;
+                    }
+                }
+            });
+        }
+    }
+    
+    public class Subscriber : ReceiveActor
+    {
+        private readonly ILoggingAdapter log = Context.GetLogger();
+
+        public Subscriber()
+        {
+            var mediator = DistributedPubSub.Get(Context.System).Mediator;
+
+            // subscribe to the topic named "content"
+            mediator.Tell(new Subscribe("content", Self));
+
+            Receive<int>(s =>
+            {
+                log.Info($"Got {s}");
+                if (s % 2 == 0)
+                {
+                    mediator.Tell(new Publish("content", ThreadLocalRandom.Current.Next(0,10)));
+                }
+            });
+
+            Receive<SubscribeAck>(subscribeAck =>
+            {
+                if (subscribeAck.Subscribe.Topic.Equals("content")
+                    && subscribeAck.Subscribe.Ref.Equals(Self)
+                    && subscribeAck.Subscribe.Group == null)
+                {
+                    log.Info("subscribing");
+                }
+            });
+        }
+    }
+    
     public class AkkaService: IHostedService
     {
         private ActorSystem _system;
@@ -67,6 +127,9 @@ namespace Akka.Cluster.Bootstrap
 
             _system = ActorSystem.Create(systemName, actorSystemSetup);
 
+            var chaos = _system.ActorOf(Props.Create<ChaosActor>(), "chaos");
+            var subscriber = _system.ActorOf(Props.Create(() => new Subscriber()), "subscriber");
+
             // start https://cmd.petabridge.com/ for diagnostics and profit
             var pbm = PetabridgeCmd.Get(_system); // start Pbm
             pbm.RegisterCommandPalette(ClusterCommands.Instance);
@@ -74,11 +137,21 @@ namespace Akka.Cluster.Bootstrap
             pbm.Start(); // begin listening for PBM management commands
             
             ClusterListener = _system.ActorOf(Akka.Cluster.Bootstrap.ClusterListener.Props(), "listener");
+
             
-            Cluster.Get(_system).RegisterOnMemberRemoved(() => {
+            _system.WhenTerminated.ContinueWith(tr =>
+            {
                 _lifetime.StopApplication(); // when the ActorSystem terminates, terminate the process
             });
+
+            var mediator = DistributedPubSub.Get(_system).Mediator;
             
+            _system.Scheduler.Advanced.ScheduleRepeatedly(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1), () =>
+            {
+               // mediator.Tell(new Publish("content", ThreadLocalRandom.Current.Next(0,10)));
+                chaos.Tell(ThreadLocalRandom.Current.Next(0,100));
+            });
+
             return Task.CompletedTask;
         }
 
